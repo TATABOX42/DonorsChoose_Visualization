6c6e3a24ca0e26f7faac2da7fcb94c5776f8b935
Benjamin Tovar <benjamin.tovarcis@gmail.com>
2015-09-09 15:56:40 -0500

    Se sube version que funciona (falta ver si fue por Bower)

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,11 +13,13 @@ wget https://s3.amazonaws.com/open_data/csv/opendata_projects.zip && unzip opend
 # mongo install database
 mongoimport -d donorschoose -c projects --type csv --file opendata_projects.csv --headerline
 
-mongo
-# in mongo terminal do
-use donorschoose
-show collections
-db.projects.findOne()
-db.projects.findOne({}, {school_state:1, resource_type:1, poverty_level:1, date_posted:1, total_donations:1, funding_status:1, _id:0})
-
-db.projects.find({}, {school_state:1, resource_type:1, poverty_level:1, date_posted:1, total_donations:1, funding_status:1, _id:0})
+# *******************
+# in mongo terminal
+# *******************
+## $ mongo
+## in mongo terminal do
+# use donorschoose
+# show collections
+# db.projects.findOne()
+# db.projects.findOne({}, {school_state:1, resource_type:1, poverty_level:1, date_posted:1, total_donations:1, funding_status:1, _id:0})
+# db.projects.find({}, {school_state:1, resource_type:1, poverty_level:1, date_posted:1, total_donations:1, funding_status:1, _id:0})
